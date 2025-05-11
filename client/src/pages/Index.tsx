cd721a3953cb96a3751e167fea824fdb0480f2e6
Md Adnan Baqi <adnan.baki786@gmail.com>
2025-05-11 06:17:23 +0530

    UI rework and Folder Formatting

---
+++ client/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-green-50">
+      <h1 className="text-5xl font-bold text-green-800">Bechdo.in</h1>
+    </div>
+  );
+};
+
+export default Index;
